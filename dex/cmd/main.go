302b4778f75b286cf35ea5235602e0c61822b5ea
Nahaq789 <naha022677@icloud.com>
2026-05-22 18:50:05 +0900

    gin不要だったのでアンインストール

--- dex/cmd/main.go
+++ dex/cmd/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "fmt"
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	router.Run(":8081")
+	fmt.Println("dex service")
 }
