2a93f8a6d9ec0c95d63c26bb8917518343b634ef
Nahaq789 <naha022677@icloud.com>
2026-06-02 14:19:56 +0900

    fix goroutine

--- box/internal/handler/grpc_handler.go
+++ box/internal/handler/grpc_handler.go
@@ -6,6 +6,7 @@ import (
 	"pob/box/proto"
 
 	"github.com/google/uuid"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -32,37 +33,43 @@ func (h *GrpcHandler) GetParty(ctx context.Context, req *proto.GetPartyRequest) 
 	}
 
 	result := make([]*proto.PartyPokemon, len(partyPokemons))
+	eg, egCtx := errgroup.WithContext(ctx)
 	for i, pp := range partyPokemons {
-		bp, err := h.boxPokemonSvc.GetById(ctx, pp.BoxPokemonId)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-		result[i] = &proto.PartyPokemon{
-			BoxPokemonId: bp.BoxPokemonId.String(),
-			Slot:         int32(pp.Slot),
-			PokemonId:    int32(bp.PokemonId),
-			Nickname:     strOrEmpty(bp.Nickname),
-			Nature:       bp.Nature,
-			AbilityId:    int32(bp.AbilityId),
-			Move1Id:      intOrZero(bp.Move1Id),
-			Move2Id:      intOrZero(bp.Move2Id),
-			Move3Id:      intOrZero(bp.Move3Id),
-			Move4Id:      intOrZero(bp.Move4Id),
-			IvHp:         int32(bp.IvHp),
-			IvAttack:     int32(bp.IvAttack),
-			IvDefense:    int32(bp.IvDefense),
-			IvSpAttack:   int32(bp.IvSpAttack),
-			IvSpDefense:  int32(bp.IvSpDefense),
-			IvSpeed:      int32(bp.IvSpeed),
-			EvHp:         int32(bp.EvHp),
-			EvAttack:     int32(bp.EvAttack),
-			EvDefense:    int32(bp.EvDefense),
-			EvSpAttack:   int32(bp.EvSpAttack),
-			EvSpDefense:  int32(bp.EvSpDefense),
-			EvSpeed:      int32(bp.EvSpeed),
-		}
+		eg.Go(func() error {
+			bp, err := h.boxPokemonSvc.GetById(egCtx, pp.BoxPokemonId)
+			if err != nil {
+				return status.Error(codes.Internal, err.Error())
+			}
+			result[i] = &proto.PartyPokemon{
+				BoxPokemonId: bp.BoxPokemonId.String(),
+				Slot:         int32(pp.Slot),
+				PokemonId:    int32(bp.PokemonId),
+				Nickname:     strOrEmpty(bp.Nickname),
+				Nature:       bp.Nature,
+				AbilityId:    int32(bp.AbilityId),
+				Move1Id:      intOrZero(bp.Move1Id),
+				Move2Id:      intOrZero(bp.Move2Id),
+				Move3Id:      intOrZero(bp.Move3Id),
+				Move4Id:      intOrZero(bp.Move4Id),
+				IvHp:         int32(bp.IvHp),
+				IvAttack:     int32(bp.IvAttack),
+				IvDefense:    int32(bp.IvDefense),
+				IvSpAttack:   int32(bp.IvSpAttack),
+				IvSpDefense:  int32(bp.IvSpDefense),
+				IvSpeed:      int32(bp.IvSpeed),
+				EvHp:         int32(bp.EvHp),
+				EvAttack:     int32(bp.EvAttack),
+				EvDefense:    int32(bp.EvDefense),
+				EvSpAttack:   int32(bp.EvSpAttack),
+				EvSpDefense:  int32(bp.EvSpDefense),
+				EvSpeed:      int32(bp.EvSpeed),
+			}
+			return nil
+		})
 	}
-
+	if err := eg.Wait(); err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
 	return &proto.PartyResponse{Pokemon: result}, nil
 }
 
