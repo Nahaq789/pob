1efee24253914770ae12275c368824ccf166d9a4
Nahaq789 <naha022677@icloud.com>
2026-06-11 11:47:52 +0900

    ref

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -3,11 +3,15 @@ package stats
 const lv = 50
 
 func CalcStats(base, iv, ev int, natureModifier float64) int {
-	f := float64(((((base * 2) + iv + ev) / 4 * lv / 100) + 5))
+	f := float64(calcBase(base, iv, ev) + 5)
 	return int(f * natureModifier)
 }
 
 func CalcHp(base, iv, ev int) int {
-	actual := ((((base * 2) + iv + ev) / 4) * lv / 100) + lv + 10
+	actual := calcBase(base, iv, ev) + lv + 10
 	return actual
+}
+
+func calcBase(base, iv, ev int) int {
+	return (base*2 + iv + ev/4) * lv / 100
 }
