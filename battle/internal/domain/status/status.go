ea291367472c7994d38f6384c7deefdd7046368a
Nahaq789 <naha022677@icloud.com>
2026-06-16 14:29:17 +0900

    status

--- battle/internal/domain/status/status.go
+++ battle/internal/domain/status/status.go
@@ -1,5 +1,15 @@
 package status
 
+import (
+	"fmt"
+	"math/rand/v2"
+	"pob/battle/internal/domain/vo"
+)
+
+var overridableStatusMoves = map[int]string{
+	173: "ねむる",
+}
+
 type Status struct {
 	main  *MainStatus
 	other []OtherStatus
@@ -14,4 +24,17 @@ func (s *Status) SetMainStatus(m *MainStatus) {
 		return
 	}
 	s.main = m
+}
+
+func (s *Status) OverrideMainStatus(m *MainStatus, moveId int) error {
+	_, ok := overridableStatusMoves[moveId]
+	if !ok {
+		return fmt.Errorf("move %d is not overridable", moveId)
+	}
+	s.main = m
+	return nil
+}
+
+func RollSleepCount() vo.Count {
+	return vo.NewCount(2 + rand.IntN(2))
 }
