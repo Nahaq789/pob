10f947968a7cb41fd5a8e6fbe95fadeffdac0762
Nahaq789 <naha022677@icloud.com>
2026-06-16 19:06:44 +0900

    fix

--- battle/internal/domain/weather/state.go
+++ battle/internal/domain/weather/state.go
@@ -2,6 +2,37 @@ package weather
 
 import "pob/battle/internal/domain/vo"
 
+type condition int
+
+const (
+	Sunny condition = iota
+	Rain
+	Sandstorm
+	Hail
+	Fog
+	Snow
+)
+
 type State struct {
+	c     condition
 	count vo.Count
+}
+
+func NewState(c condition, count vo.Count) State {
+	return State{c: c, count: count}
+}
+
+func (w State) C() condition {
+	return w.c
+}
+
+func (w State) Count() vo.Count {
+	return w.count
+}
+
+func (w State) Decrement() State {
+	return State{
+		c:     w.c,
+		count: w.count.Decrement(),
+	}
 }
