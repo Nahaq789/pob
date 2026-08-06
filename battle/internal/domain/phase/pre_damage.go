91c51ed3e594fab23299cbc5c6ba382f637d0de7
Nahaq789 <naha022677@icloud.com>
2026-08-06 19:19:46 +0900

    pre

--- battle/internal/domain/phase/pre_damage.go
+++ battle/internal/domain/phase/pre_damage.go
@@ -1,1 +1,14 @@
 package phase
+
+import "pob/battle/internal/domain/battle"
+
+type PreDamagePhaseHandler struct {
+}
+
+func NewPreDamagePhaseHandler() *PreDamagePhaseHandler {
+	return &PreDamagePhaseHandler{}
+}
+
+func (pre *PreDamagePhaseHandler) Handle(b *battle.Battle) (map[string][]string, error) {
+	return nil, nil
+}
